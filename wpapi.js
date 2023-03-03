4e7ab8c46d5b58d9d11e0d154c99b532c465b01c
tayyab0564 <119144854+tayyab0564@users.noreply.github.com>
2023-03-03 19:01:09 +0500

    Update wpapi.js

--- wpapi.js
+++ wpapi.js
@@ -1,3 +1,3 @@
 import WPAPI from 'wpapi';
 
-export const wp = new WPAPI({ endpoint: 'http://your-wordpress-site-url.com/wp-json' });
+export const wp = new WPAPI({ endpoint: 'http://zhobtimes.com.com/wp-json' });
